86c27ddfe4699eed3081674c79f007962abf622d
Ashish Yadav <ashish@Ashishs-MacBook-Air.local>
2024-09-21 17:57:38 -0700

    first commit

---
+++ src/main/java/edu/depaul/cdm/se452/lms/LmsApplication.java
@@ -0,0 +1,13 @@
+package edu.depaul.cdm.se452.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/depaul/cdm/se452/lms/LmsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.depaul.cdm.se452.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
