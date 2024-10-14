f7ec9bba939c6fc07391f7895f24587dafa9bc72
Dhairya4256 <167940207+Dhairya4256@users.noreply.github.com>
2024-10-15 01:23:01 +0530

    Update package-info.java

--- src/main/java/edu/depaul/cdm/se452/discussionforum/package-info.java
+++ src/main/java/edu/depaul/cdm/se452/discussionforum/package-info.java
@@ -1,8 +1,8 @@
 /*
  * @author : Dhairya Patel
- *
+ * 
  * Milestone 2:
- *
+ * File: discussionforum:
  * 1. Entity Classes:
  *    - Created `Discussion`, `Comment`, and `Reply` entities with fields like `content`, `createdBy`, and `createdAt`.
  *    - Used Lombok's `@Data` annotation to automatically generate getters, setters, and other methods.
