cc160754b05b0683c7f8494fac3699cdf326551f
Shaun Jiji <shaunjiji123@gmail.com>
2024-10-14 12:20:27 -0500

    complete notifcation package entities and repositories

---
+++ src/main/java/edu/depaul/cdm/se452/notification/AssignmentNotificationRepository.java
@@ -0,0 +1,4 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AssignmentNotificationRepository extends JpaRepository<AssignmentNotification, Integer> {
+}
--- src/main/java/edu/depaul/cdm/se452/notification/Notification.java
+++ src/main/java/edu/depaul/cdm/se452/notification/Notification.java
@@ -1,31 +1,37 @@
 package edu.depaul.cdm.se452.notification;
 import lombok.*;
 import javax.persistence.*;
-import java.sql.Timestamp;
+import java.util.List;
 
 @Entity
 @Table(name = "Notification")
-@Data // Generates getters, setters, toString, equals, and hashCode methods
-@NoArgsConstructor // Generates a no-args constructor
-@AllArgsConstructor // Generates an all-args constructor
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Notification {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer notificationId;
 
     @Column(name = "user_id", nullable = false)
-    private Integer userId;
+    private Integer userId; // ID of the user receiving the notification
 
     @Column(name = "message", nullable = false)
-    private String message;
+    private String message; // Notification message
 
     @Column(name = "notification_type", length = 50)
-    private String notificationType;
+    private String notificationType; // Type of notification ('assignment' or 'grade')
 
-    @Column(name = "created_date")
-    private Timestamp createdDate = new Timestamp(System.currentTimeMillis());
+    @Column(name = "created_date", nullable = false, updatable = false)
+    private Timestamp createdDate = new Timestamp(System.currentTimeMillis()); // When the notification was created
 
-    @Column(name = "is_read")
-    private Boolean isRead = false;
+    @Column(name = "is_read", nullable = false)
+    private Boolean isRead = false; // Has the notification been read?
+
+    @OneToMany(mappedBy = "notification", cascade = CascadeType.ALL)
+    private List<AssignmentNotification> assignmentNotifications;
+
+    @OneToMany(mappedBy = "notification", cascade = CascadeType.ALL)
+    private List<GradingNotification> gradingNotifications;
 }
