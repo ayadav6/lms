da3ed30b4126a2e41ba9946586ff4568902506fc
Pawan <ssamala1@depaul.edu>
2024-10-14 16:08:33 -0500

    Assessment Module changes

--- src/main/java/edu/depaul/cdm/se452/assessment/package-info.java
+++ src/main/java/edu/depaul/cdm/se452/assessment/package-info.java
@@ -2,10 +2,19 @@ package edu.depaul.cdm.se452.assessment;
 
 /*
  * @author : Sweta Yellayya Samala
- * Assessment module 
- *  --Assignment
- *  --Submission
- *  --Grade
+ * 
+ * MODULE: Assessment and Grading
+ * Provides the classes and interfaces for managing assessments and grading in the Learning Management System (LMS)
+ * 
+ * Key components include:
+ *  --Assignment: for managing assignments. 
+ *  --Submission: for handling student submissions.
+ *  --Grade: for recording grades given by instructors.
+ * 
+ * This would interact with below modules to provide a cohesive learning experience.:
+ *  --User
+ *  --Course modules
+ * 
  */
 
  
