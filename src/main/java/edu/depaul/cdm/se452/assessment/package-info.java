9340246ebf5a36964f919964d0bdf82df50b8f2b
ssamala1-code <ssamala1@depaul.edu>
2024-10-15 01:47:09 -0500

    Update package-info.java

--- src/main/java/edu/depaul/cdm/se452/assessment/package-info.java
+++ src/main/java/edu/depaul/cdm/se452/assessment/package-info.java
@@ -6,14 +6,21 @@ package edu.depaul.cdm.se452.assessment;
  * MODULE: Assessment and Grading
  * Provides the classes and interfaces for managing assessments and grading in the Learning Management System (LMS)
  * 
+ * 1. Entity Classes:
  * Key components include:
  *  --Assignment: for managing assignments. 
  *  --Submission: for handling student submissions.
  *  --Grade: for recording grades given by instructors.
  * 
+ * 2. Repository Layer:
+ *  --Created repositories ('AssignmentRepository', 'SubmissionRepository', 'GradeRepository')
+ * 
+ * 3. Database Setup:
+ * 
+ * 4. Interaction with Other Modules:
  * This would interact with below modules to provide a cohesive learning experience.:
- *  --User
- *  --Course modules
+ *  --User module
+ *  --Course module
  * 
  */
 
