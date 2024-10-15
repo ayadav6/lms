dacb5c84cd1e75c0ea7f7faa615407833cd29c8a
Ashish Yadav <ashish@Ashishs-MacBook-Air.local>
2024-10-14 21:19:54 -0700

    Changes in main class

--- src/main/java/edu/depaul/cdm/se452/lms/LmsApplication.java
+++ src/main/java/edu/depaul/cdm/se452/lms/LmsApplication.java
@@ -3,7 +3,7 @@ package edu.depaul.cdm.se452.lms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"edu.depaul.cdm.se452"})
 public class LmsApplication {
 
 	public static void main(String[] args) {
