333ab824e521af1fbb6962e503e897accd5808e8
Dhairya4256 <167940207+Dhairya4256@users.noreply.github.com>
2024-10-15 07:48:38 +0530

    Update package-info.java

--- src/main/java/edu/depaul/cdm/se452/discussionforum/package-info.java
+++ src/main/java/edu/depaul/cdm/se452/discussionforum/package-info.java
@@ -14,6 +14,19 @@
  * 3. Database Setup:
  *    - Configured H2 database.
  *
+ * 4. Version Update:
+ *    - Updated the project version to `0.2`
+ *
+ * 5. Testing:
+ *    - Used the H2 database for development and tested the persistence of or our project 'lms'
+ *
+ * 6. Relationships with Other Components:
+ *    - The `Discussion`, `Comment`, and `Reply` entities are closely related to the User and Course components:
+ *       - Discussions and comments are linked to courses through the `Course` entity.
+ *       - Users (instructors or students) can create discussions, post comments, and replies through the `User` entity.
+ *       - The `createdBy` field in `Post` refers to the User entity, establishing the relationship between discussions/comments and users.
+ *       - The `Discussion` entity is linked to a specific course via the `course_id` field.
+ *
  *
  *
  */
