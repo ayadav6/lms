3a0a7d50be367f9c22a2a5a47999c0c89de1024d
Dhairya4256 <167940207+Dhairya4256@users.noreply.github.com>
2024-10-16 12:08:06 +0530

    Update LmsApplication.java

--- src/main/java/edu/depaul/cdm/se452/lms/LmsApplication.java
+++ src/main/java/edu/depaul/cdm/se452/lms/LmsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"edu.depaul.cdm.se452"})
 @EnableJpaRepositories(basePackages = "edu.depaul.cdm.se452.user")
-@EntityScan(basePackages = {"edu.depaul.cdm.se452.user", "edu.depaul.cdm.se452.course", "edu.depaul.cdm.se452.assessment", "edu.depaul.cdm.se452.discussionforum", "edu.depaul.cdm.se452.notification"}) // Explicitly scan both User and Course entities
+@EntityScan(basePackages = {"edu.depaul.cdm.se452.user", "edu.depaul.cdm.se452.course", "edu.depaul.cdm.se452.assessment", "edu.depaul.cdm.se452.discussionforum", "edu.depaul.cdm.se452.notification"}) 
 public class LmsApplication {
 
 	public static void main(String[] args) {
