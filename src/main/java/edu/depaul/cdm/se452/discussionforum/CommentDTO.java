b02a6948c1798a01e4b47dd821ccacec41c22190
Dhairya4256 <patel.dhairya4256@gmail.com>
2024-11-19 09:41:40 +0530

    My changes for Final Submission.

--- src/main/java/edu/depaul/cdm/se452/discussionforum/CommentDTO.java
+++ src/main/java/edu/depaul/cdm/se452/discussionforum/CommentDTO.java
@@ -1,11 +1,18 @@
 package edu.depaul.cdm.se452.discussionforum;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CommentDTO {
     private Long id;
+
+    @NotNull(message = "Content is required")
+    @Size(min = 5, max = 500, message = "Content must be between 5 and 500 characters")
     private String content;
+
     private Long createdByUserId;
     private Long discussionId;
 }
+
